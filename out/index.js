6929574ad73439db036fdbfab328da74f15688f5
Josh Aksamit <joshua.aksamit@gmail.com>
2024-07-01 23:13:33 -0700

    js functionality complete

--- out/index.js
+++ out/index.js
@@ -1,1 +1,94 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "51b48abd4a7425f1ded7d7ebc6f465ae";
+weatherForm.addEventListener("submit", (event) => __awaiter(this, void 0, void 0, function* () {
+    event.preventDefault();
+    const city = cityInput.value;
+    if (city) {
+        try {
+            const weatherData = yield getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch (error) {
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else {
+        displayError("Please enter a city");
+    }
+}));
+function getWeatherData(city) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        const response = yield fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error("Could not fetch weather data");
+        }
+        return yield response.json();
+    });
+}
+function displayWeatherInfo(data) {
+    const { name: city, main: { temp, humidity }, weather: [{ description, id }] } = data;
+    card.textContent = "";
+    card.style.display = "flex";
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${((temp - 273.15) * (9 / 5) + 32).toFixed(1)}°F`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
+}
+function getWeatherEmoji(weatherId) {
+    switch (true) {
+        case (weatherId >= 200 && weatherId < 300):
+            return "⛈️";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case (weatherId >= 400 && weatherId < 500):
+            return "🌧️";
+        case (weatherId >= 500 && weatherId < 600):
+            return "❄️";
+        case (weatherId >= 700 && weatherId < 800):
+            return "🌫️";
+        case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "❓";
+    }
+}
+function displayError(message) {
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = "";
+    errorDisplay.classList.add("errorDisplay");
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
 //# sourceMappingURL=index.js.map
